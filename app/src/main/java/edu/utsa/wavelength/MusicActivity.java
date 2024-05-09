5b0f85834b967d727dd76d448947cade55fb0991
Nancbanc <156459884+Nancbanc@users.noreply.github.com>
2024-05-09 12:22:53 -0500

    yep

---
+++ app/src/main/java/edu/utsa/wavelength/MusicActivity.java
@@ -0,0 +1,14 @@
+package edu.utsa.wavelength;
+
+import android.os.Bundle;
+
+import androidx.activity.ComponentActivity;
+
+public class MusicActivity extends ComponentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.musicplayer);
+    }
+}
