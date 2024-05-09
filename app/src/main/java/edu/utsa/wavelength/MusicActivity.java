59d719e2550802e5fbde1f07c0996b592b435059
Nancbanc <156459884+Nancbanc@users.noreply.github.com>
2024-05-09 13:22:12 -0500

    Update MusicActivity.java

--- app/src/main/java/edu/utsa/wavelength/MusicActivity.java
+++ app/src/main/java/edu/utsa/wavelength/MusicActivity.java
@@ -1,5 +1,6 @@
 package edu.utsa.wavelength;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
@@ -10,9 +11,22 @@ import androidx.activity.ComponentActivity;
 
 public class MusicActivity extends ComponentActivity {
 
+    ImageView img;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.musicplayer);
+
+        img = findViewById(R.id.playpause);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mp = MediaPlayer.create(MusicActivity.this, R.raw.shake);
+                mp.start();
+
+                //return false;
+            }
+        });
     }
 }
