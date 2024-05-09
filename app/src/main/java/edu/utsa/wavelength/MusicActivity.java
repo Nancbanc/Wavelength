38bbd5562861e312896fc199d62f6229ad232298
Nancbanc <156459884+Nancbanc@users.noreply.github.com>
2024-05-09 13:15:16 -0500

    commit

--- app/src/main/java/edu/utsa/wavelength/MusicActivity.java
+++ app/src/main/java/edu/utsa/wavelength/MusicActivity.java
@@ -1,14 +1,24 @@
 package edu.utsa.wavelength;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.ComponentActivity;
 
 public class MusicActivity extends ComponentActivity {
 
+    ImageView img;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.musicplayer);
+
+        img = findViewById(R.id.playpause);
+        img.setOnTouchListener(new View.OnTouchListener() {
+
+        }
+
     }
 }
+
+
