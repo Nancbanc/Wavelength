e27a0b8b86f0a8ec8f500522b6699dd45aa04f2d
Kiahna Isadore <kiahnaisadore@gmail.com>
2024-05-09 17:28:01 -0500

    updated buttons

--- app/src/main/java/edu/utsa/wavelength/MusicActivity.java
+++ app/src/main/java/edu/utsa/wavelength/MusicActivity.java
@@ -1,9 +1,11 @@
 package edu.utsa.wavelength;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.activity.ComponentActivity;
@@ -15,11 +17,15 @@ public class MusicActivity extends ComponentActivity {
     ImageView backButton;
     boolean isPlaying = false;
     MediaPlayer musicPlayer;
+    private int currentSongIndex = 0;
+    int[] songCovers = {R.drawable.md, R.drawable.snooze, R.drawable.whatevershewants};
+    int[] songResources = {R.raw.million, R.raw.whatever, R.raw.snooze};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.musicplayer);
+        setupButtons();
 
         img = findViewById(R.id.playpause);
         skipButton = findViewById(R.id.skip);
@@ -72,17 +78,46 @@ public class MusicActivity extends ComponentActivity {
     }
 
     private void playNextSong() {
+<<<<<<< Updated upstream
        // musicPlayer.stop();
         musicPlayer = MediaPlayer.create(this, R.raw.whatever);
+=======
+        currentSongIndex = (currentSongIndex + 1) % songCovers.length;
+        musicPlayer.stop();
+        musicPlayer = MediaPlayer.create(this, getSongResource(currentSongIndex));
+        //musicPlayer = MediaPlayer.create(this, R.raw.whatever);
+>>>>>>> Stashed changes
         musicPlayer.start();
+
+        updateSongCover();
+
         isPlaying = true;
     }
 
+    private void updateSongCover(){
+        int currentIndex = getCurrentSongIndex();
+        img.setImageResource(songCovers[currentIndex]);
+    }
+    private int getCurrentSongIndex(){
+        return currentSongIndex;
+    }
+
     private void playPreviousSong() {
+<<<<<<< Updated upstream
        // musicPlayer.stop();
         musicPlayer = MediaPlayer.create(this, R.raw.snooze);
+=======
+        currentSongIndex = (currentSongIndex - 1 + songCovers.length) % songCovers.length;
+        musicPlayer.stop();
+        musicPlayer = MediaPlayer.create(this, getSongResource(currentSongIndex));
+>>>>>>> Stashed changes
         musicPlayer.start();
+        updateSongCover();
         isPlaying = true;
+    }
+
+    private int getSongResource(int index){
+        return songResources[index];
     }
 
     @Override
@@ -92,5 +127,24 @@ public class MusicActivity extends ComponentActivity {
             musicPlayer.release();
             musicPlayer = null;
         }
+    }
+
+    private void setupButtons() {
+
+        ImageButton button1 = (ImageButton) findViewById(R.id.musichome);
+        ImageButton button2 = (ImageButton) findViewById(R.id.playlist);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MusicActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MusicActivity.this, PlaylistManagement.class);
+                startActivity(intent);
+            }
+        });
     }
 }
